f1e9a64cf91ecd68ab8b5148209dd51ed12d2ed0
Jean-Michael Diei <jeanmichaeldiei@yahoo.com>
2017-01-21 16:32:18 -0500

    Changes to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,127 +1,128 @@
 
-    var express = require('express');
-    var router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-    //Requires for spell-checker
-    var dictionary = require('dictionary-en-us');
-    var nspell = require('nspell');
+//Requires for spell-checker
+var dictionary = require('dictionary-en-us');
+var nspell = require('nspell');
 
-    /* GET home page. */
-    var watson = require('watson-developer-cloud');
+/* GET home page. */
+var watson = require('watson-developer-cloud');
 
-    var tone_analyzer = new watson.ToneAnalyzerV3({
-      username: 'cdedd54c-683f-4e0a-848c-262a02f354e8',
-      password: 'ypYZZkbvj6bc',
-      version: 'v3',
-      version_date: '2016-05-19 '
-    });
+var tone_analyzer = new watson.ToneAnalyzerV3({
+    username: 'cdedd54c-683f-4e0a-848c-262a02f354e8',
+    password: 'ypYZZkbvj6bc',
+    version: 'v3',
+    version_date: '2016-05-19 '
+});
 
-    router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
 
-         res.render('editor', { emotionArray: [],languageArray: [], socialArray: []});
+    res.render('editor', { emotionArray: [],languageArray: [], socialArray: []});
 
-    });
+});
 
 
-    router.post('/add', function(req, res, next) {
+router.post('/add', function(req, res, next) {
+    var ratio = 0.0;
+    var numWords = 0;
+    var numMisspelled = 0;
+    tone_analyzer.tone({ text: req.body.txt },
 
-        tone_analyzer.tone({ text: req.body.txt },
+        dictionary(function (err, dict) {
+            if (err) {
+                throw err;
+            }
 
-                    dictionary(function (err, dict) {
-                      if (err) {
-                        throw err;
-                      }
+            var spell = nspell(dict);
+            var email = req.body.txt;
+            var punctuationless = email.replace(/[.,\/#!$%\^&\*;:{}=\_`~()]/g,"");
+            var finalString = punctuationless.replace(/\s{2,}/g," ");
+            var words = finalString.split(" ");
 
-                      var spell = nspell(dict);
-                      var email = req.body.txt;
-                      var punctuationless = email.replace(/[.,\/#!$%\^&\*;:{}=\_`~()]/g,"");
-                      var finalString = punctuationless.replace(/\s{2,}/g," ");
-                      var words = finalString.split(" ");
+             numWords = words.length;
 
-                      var numWords = words.length;
-                      var misspelled = 0;
+            for(var letter=65;letter<91;letter++)
+            {
+                var _char = String.fromCharCode(letter);
+                spell.remove(_char);
+            }
+            for(var letter=97;letter<=122;letter++)
+            {
+                var _char = String.fromCharCode(letter);
+                spell.remove(_char);
+            }
 
-                      for(var letter=65;letter<91;letter++)
-                      {
-                          var _char = String.fromCharCode(letter);
-                          spell.remove(_char);
-                      }
-                      for(var letter=97;letter<=122;letter++)
-                      {
-                          var _char = String.fromCharCode(letter);
-                          spell.remove(_char);
-                      }
+            spell.add('A');
+            spell.add('a');
+            spell.add('i');
+            spell.add('I');
+            console.log(words);
 
-                      spell.add('A');
-                      spell.add('a');
-                      spell.add('i');
-                      spell.add('I');
-                      console.log(words);
-
-                      words.forEach(function(obj) {
-                            if(spell.correct(obj) == false) {
-                                console.log(obj);
-                                misspelled++;
-                            }
-                      })
-                      var ratio = misspelled/numWords;
-                      console.log("There are " + misspelled + " misspelled words out of " + numWords + " words. Ratio: " + ratio);
-                       res.render('editor', { misspelled: misspelled,ratio: ratio, numWords: numWords});
-                   }),
+            words.forEach(function(obj) {
+                if(spell.correct(obj) == false) {
+                    console.log(obj);
+                    numMisspelled++;
+                }
+            })
+            ratio = numMisspelled/numWords;
+            //console.log("There are " + misspelled + " misspelled words out of " + numWords + " words. Ratio: " + ratio);
+            //res.render('editor', { misspelled: misspelled,ratio: ratio, numWords: numWords});
+        }),
 
         function(err, tone) {
             if (err)
-              console.log(err);
+            console.log(err);
             else {
-              console.log(JSON.stringify(tone, null, 2) + "\n\n\n");
-              var object = tone["document_tone"]["tone_categories"][0]["tones"][0]["tone_name"]
-             //console.log(JSON.stringify(object, null, 2));
+                console.log(JSON.stringify(tone, null, 2) + "\n\n\n");
+                var object = tone["document_tone"]["tone_categories"][0]["tones"][0]["tone_name"]
+                //console.log(JSON.stringify(object, null, 2));
 
-             var emotionArray = [];
-             var languageArray = [];
-             var socialArray = [];
+                var emotionArray = [];
+                var languageArray = [];
+                var socialArray = [];
 
-             var emotion = tone["document_tone"]["tone_categories"][0]["tones"]
+                var emotion = tone["document_tone"]["tone_categories"][0]["tones"]
 
-             emotion.forEach(function(obj){
-                 var newObj = {
-                     name: obj["tone_name"],
-                     value: obj["score"]*100
-                 }
-                 emotionArray.push(newObj);
-             });
+                emotion.forEach(function(obj){
+                    var newObj = {
+                        name: obj["tone_name"],
+                        value: obj["score"]*100
+                    }
+                    emotionArray.push(newObj);
+                });
 
-             var language = tone["document_tone"]["tone_categories"][1]["tones"]
+                var language = tone["document_tone"]["tone_categories"][1]["tones"]
 
-             language.forEach(function(obj){
-                 var newObj = {
-                     name: obj["tone_name"],
-                     value: obj["score"]*100
-                 }
-                 languageArray.push(newObj);
-             });
+                language.forEach(function(obj){
+                    var newObj = {
+                        name: obj["tone_name"],
+                        value: obj["score"]*100
+                    }
+                    languageArray.push(newObj);
+                });
 
-             var social = tone["document_tone"]["tone_categories"][2]["tones"]
+                var social = tone["document_tone"]["tone_categories"][2]["tones"]
 
-             social.forEach(function(obj){
-                 var newObj = {
-                     name: obj["tone_name"],
-                     value: obj["score"]*100
-                 }
-                 socialArray.push(newObj);
-             });
+                social.forEach(function(obj){
+                    var newObj = {
+                        name: obj["tone_name"],
+                        value: obj["score"]*100
+                    }
+                    socialArray.push(newObj);
+                });
 
-             emotionArray.forEach(function(obj){
-                 console.log(obj.name +  " - " + obj.value);
-             });
-             languageArray.forEach(function(obj){
-                 console.log(obj.name +  " - " + obj.value);
-             });
-             socialArray.forEach(function(obj){
-                 console.log(obj.name +  " - " + obj.value);
-             });
-               res.render('editor', { emotionArray: emotionArray,languageArray: languageArray, socialArray: socialArray});
-         }
+                emotionArray.forEach(function(obj){
+                    console.log(obj.name +  " - " + obj.value);
+                });
+                languageArray.forEach(function(obj){
+                    console.log(obj.name +  " - " + obj.value);
+                });
+                socialArray.forEach(function(obj){
+                    console.log(obj.name +  " - " + obj.value);
+                });
+                res.render('editor', { emotionArray: emotionArray,languageArray: languageArray, socialArray: socialArray, numMisspelled: numMisspelled,ratio: ratio, numWords: numWords});
+            }
         });
 
     });
