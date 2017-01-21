dcecf5bef716f6681e93cc37ec58668bd7a5a64b
Dhruv Upadhyay <1997dhruv@gmail.com>
2017-01-21 04:43:40 -0500

    displaying data on front end

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-var watson = require('watson-developer-cloud/tone_analyzer/v3');
+var watson = require('watson-developer-cloud');
 
-var tone_analyzer = new ToneAnalyzerV3({
+var tone_analyzer = new watson.ToneAnalyzerV3({
   username: 'cdedd54c-683f-4e0a-848c-262a02f354e8',
   password: 'ypYZZkbvj6bc',
   version: 'v3',
@@ -13,7 +13,14 @@ var tone_analyzer = new ToneAnalyzerV3({
 
 router.get('/', function(req, res, next) {
 
-    tone_analyzer.tone({ text: 'A word is dead when it is said, some say. Emily Dickinson' },
+     res.render('editor', { emotionArray: [],languageArray: [], socialArray: []});
+
+});
+
+
+router.post('/add', function(req, res, next) {
+ 
+    tone_analyzer.tone({ text: req.body.txt },
       function(err, tone) {
         if (err)
           console.log(err);
@@ -21,7 +28,7 @@ router.get('/', function(req, res, next) {
           console.log(JSON.stringify(tone, null, 2) + "\n\n\n");
           var object = tone["document_tone"]["tone_categories"][0]["tones"][0]["tone_name"]
          //console.log(JSON.stringify(object, null, 2));
-
+         
          var emotionArray = [];
          var languageArray = [];
          var socialArray = [];
@@ -60,7 +67,7 @@ router.get('/', function(req, res, next) {
              console.log(obj.name +  " - " + obj.value);
 
          });
-           res.render('index', { title: 'Express', emotionArray: emotionArray,languageArray: languageArray, socialArray: socialArray});
+           res.render('editor', { emotionArray: emotionArray,languageArray: languageArray, socialArray: socialArray});
      }
     });
 
